163151510de2e0ee558381621960889c6a330f56
Davon <tie-n-dev@ntymail.com>
2018-08-05 20:16:26 +0200

    Create Measure entity

---
+++ Calculator/Calculator.DAL/Global/Entities/Measure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.DAL.Global.Entities
+{
+    public class Measure
+    {
+        private int measureID;
+        /// <summary>
+        /// Clé primaire de la table "measure"
+        /// </summary>
+        public int MeasureID
+        {
+            get { return measureID; }
+            set { measureID = value; }
+        }
+
+        private DateTime date;
+        /// <summary>
+        /// Date de la mesure
+        /// </summary>
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+    }
+}

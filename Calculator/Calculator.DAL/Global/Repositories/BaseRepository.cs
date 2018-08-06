8609a6ad0c31724f2fac86acc8db05f44282ee58
Davon <tie-n-dev@ntymail.com>
2018-08-06 19:58:16 +0200

    Base Repository done

--- Calculator/Calculator.DAL/Global/Repositories/BaseRepository.cs
+++ Calculator/Calculator.DAL/Global/Repositories/BaseRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Calculator.DAL.Global.Repositories
 {
-    public class BaseRepository<Tkey, TEntity>
+    public abstract class BaseRepository<Tkey, TEntity>
     {
         protected SqlConnection db;
         private string instanceServer = @"(local)\SQL";
@@ -38,13 +38,64 @@ namespace Calculator.DAL.Global.Repositories
         }
 
         
-        public IEnumerable<TEntity> GetAll()
+        public List<TEntity> GetAll()
         {
-            SqlCommand cmd = new SqlCommand("SELECT * FROM {TableName}");
+            List<TEntity> Items = new List<TEntity>();
 
-            IEnumerable<TEntity> list
-            return 
+            SqlCommand cmd = db.CreateCommand();
+            cmd.CommandText = $"SELECT * FROM {TableName}";
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            db.Open();
+            while (reader.Read())
+            {
+                TEntity entity = ReaderToClient(reader);
+
+                Items.Add(entity);
+            }
+            reader.Close();
+
+            db.Close();
+
+            return Items; 
         }
 
+        public TEntity GetOne(Tkey id)
+        {
+            TEntity item = default(TEntity);
+
+            SqlCommand cmd = db.CreateCommand();
+            cmd.CommandText = $"SELECT @{id} FROM {TableName}";
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            db.Open();
+            if (reader.Read())
+            {
+                item = ReaderToClient(reader);
+            }
+            reader.Close();
+
+            db.Close();
+
+            return item;
+        }
+
+        public bool Delete(Tkey id)
+        {
+            SqlCommand cmd = db.CreateCommand();
+            cmd.CommandText = $"DELETE FROM {TableName} WHERE {TableID} = @{id}";
+            cmd.Parameters.AddWithValue("@id", id);
+
+            db.Open();
+            int isDeleted = cmd.ExecuteNonQuery();
+            db.Close();
+
+            return isDeleted == 1;
+        }
+
+        protected abstract TEntity ReaderToClient(SqlDataReader reader);
+        public abstract bool Insert(TEntity entity);
+        public abstract bool Update(TEntity entity);
     }
 }

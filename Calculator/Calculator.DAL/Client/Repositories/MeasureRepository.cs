465ff644338ef3786a3aea75bff73d51d4b20981
Davon <tie-n-dev@ntymail.com>
2018-08-07 22:58:07 +0200

    Create Measure repository client

---
+++ Calculator/Calculator.DAL/Client/Repositories/MeasureRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GR = Calculator.DAL.Global.Repositories;
+using G = Calculator.DAL.Global.Entities;
+using C = Calculator.DAL.Client.Entities;
+using Calculator.DAL.Mapper;
+
+namespace Calculator.DAL.Client.Repositories
+{
+    public class MeasureRepository
+    {
+        private MeasureRepository instance;
+        public MeasureRepository Instance
+        {
+            get { return instance ?? (instance = new MeasureRepository()); }
+        }
+
+        private GR.MeasureRepository MeasureRepo;
+
+        public MeasureRepository()
+        {
+            MeasureRepo = new GR.MeasureRepository();
+        }
+
+
+        public C.Measure GetOne(int id)
+        {
+            return MeasureMapper.ToClient(MeasureRepo.GetOne(id));
+        }
+
+        public List<C.Measure> GetAll()
+        {
+            return MeasureMapper.ToClient(MeasureRepo.GetAll());
+        }
+
+        public int Insert(C.Measure entity)
+        {
+            return MeasureRepo.Insert(MeasureMapper.ToGlobal(entity));
+        }
+
+        public bool Update(C.Measure entity)
+        {
+            return MeasureRepo.Update(MeasureMapper.ToGlobal(entity));
+        }
+
+        public bool Delete(int id)
+        {
+            return MeasureRepo.Delete(id);
+        }
+    }
+}

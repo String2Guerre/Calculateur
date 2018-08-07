a7c714013944a7b0bc169a4f3d3c0928d66a73fd
Davon <tie-n-dev@ntymail.com>
2018-08-07 17:49:45 +0200

    Create MeasureType mapper

---
+++ Calculator/Calculator.DAL/Mapper/MeasureTypeMapper.cs
@@ -0,0 +1,59 @@
+﻿using G = Calculator.DAL.Global.Entities;
+using C = Calculator.DAL.Client.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.DAL.Mapper
+{
+    public class MeasureTypeMapper
+    {
+        public G.MeasureType ToGlobal(C.MeasureType entity)
+        {
+            return new G.MeasureType()
+            {
+                MeasureTypeID = entity.MeasureTypeID,
+                Name = entity.Name,
+                Value = entity.Value,
+                MeasureID = entity.MeasureID
+            };
+        }
+
+        public List<G.MeasureType> ToGlobal(List<C.MeasureType> entities)
+        {
+            List<G.MeasureType> measureTypes = new List<G.MeasureType>();
+
+            foreach (C.MeasureType item in entities)
+            {
+                measureTypes.Add(ToGlobal(item));
+            }
+
+            return measureTypes;
+        }
+
+        public C.MeasureType ToClient(G.MeasureType entity)
+        {
+            return new C.MeasureType()
+            {
+                MeasureTypeID = entity.MeasureTypeID,
+                Name = entity.Name,
+                Value = entity.Value,
+                MeasureID = entity.MeasureID
+            };
+        }
+
+        public List<C.MeasureType> ToClient(List<G.MeasureType> entities)
+        {
+            List<C.MeasureType> measureTypes = new List<C.MeasureType>();
+
+            foreach (G.MeasureType item in entities)
+            {
+                measureTypes.Add(ToClient(item));
+            }
+
+            return measureTypes;
+        }
+    }
+}

b3d6a4dded7edc3da9712520c71f8f8b055ad51a
Davon <tie-n-dev@ntymail.com>
2018-08-07 21:49:37 +0200

    Create User repository client

---
+++ Calculator/Calculator.DAL/Client/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GR = Calculator.DAL.Global.Repositories;
+using G = Calculator.DAL.Global.Entities;
+using C = Calculator.DAL.Client.Entities;
+using Calculator.DAL.Mapper;
+
+namespace Calculator.DAL.Client.Repositories
+{
+    public class UserRepository
+    {
+        private UserRepository instance;
+        public UserRepository Instance
+        {
+            get { return instance ?? (instance = new UserRepository()); }
+        }
+
+        private GR.UserRepository UserRepo;
+
+        public UserRepository()
+        {
+            UserRepo = new GR.UserRepository();
+        }
+
+       
+    }
+}

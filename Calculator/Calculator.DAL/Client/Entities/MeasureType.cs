a2adf78a6a4b257f741fa883afd5b209f0f53fab
Davon <tie-n-dev@ntymail.com>
2018-08-07 16:32:08 +0200

    Create User mapper

---
+++ Calculator/Calculator.DAL/Client/Entities/MeasureType.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.DAL.Client.Entities
+{
+    public class MeasureType
+    {
+        private int measureTypeID;
+        /// <summary>
+        /// Clé primaire de la table "measure_type"
+        /// </summary>
+        public int MeasureTypeID
+        {
+            get { return measureTypeID; }
+            set { measureTypeID = value; }
+        }
+
+        private string name;
+        /// <summary>
+        /// Nom de ce qui est mesuré (Tour de taille, tour de poitrine, tour de bras, ...)
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private double value;
+        /// <summary>
+        /// Valeur de ce qui est mesuré
+        /// </summary>
+        public double Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        private int measureID;
+        /// <summary>
+        /// Clé étrangère de la table "mesure"
+        /// </summary>
+        public int MeasureID
+        {
+            get { return measureID; }
+            set { measureID = value; }
+        }
+
+        public MeasureType() { }
+
+        public MeasureType(string name, double value, int measureID)
+        {
+            this.Name = name;
+            this.Value = value;
+            this.MeasureID = measureID;
+        }
+    }
+}
---
+++ Calculator/Calculator.DAL/Mapper/UserMapper.cs
@@ -0,0 +1,65 @@
+﻿using G = Calculator.DAL.Global.Entities;
+using C = Calculator.DAL.Client.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.DAL.Mapper
+{
+    public class UserMapper
+    {
+        public G.User ToGlobal(C.User entity)
+        {
+            return new G.User()
+            {
+                UserID = entity.UserID,
+                Name=entity.Name,
+                Email=entity.Email,
+                Pwd=entity.Pwd,
+                Gender=entity.Gender,
+                Birthdate=entity.Birthdate,
+                InitialWeight=entity.InitialWeight
+            };
+        }
+
+        public List<G.User> ToGlobal(List<C.User> entities)
+        {
+            List<G.User> users = new List<G.User>();
+
+            foreach (C.User item in entities)
+            {
+                users.Add(ToGlobal(item));
+            }
+
+            return users;
+        }
+
+        public C.User ToCLient(G.User entity)
+        {
+            return new C.User()
+            {
+                UserID = entity.UserID,
+                Name = entity.Name,
+                Email = entity.Email,
+                Pwd = entity.Pwd,
+                Gender = entity.Gender,
+                Birthdate = entity.Birthdate,
+                InitialWeight = entity.InitialWeight
+            };
+        }
+
+        public List<C.User> ToClient(List<G.User> entities)
+        {
+            List<C.User> users = new List<C.User>();
+
+            foreach (G.User item in entities)
+            {
+                users.Add(ToCLient(item));
+            }
+
+            return users;
+        }
+    }
+}

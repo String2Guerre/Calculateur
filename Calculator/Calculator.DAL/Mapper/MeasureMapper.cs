c1b2d5583617f1db7edcf4e453522ffdc7c37a7d
Davon <tie-n-dev@ntymail.com>
2018-08-07 17:39:41 +0200

    Create Measure mapper

---
+++ Calculator/Calculator.DAL/Mapper/MeasureMapper.cs
@@ -0,0 +1,55 @@
+﻿using G = Calculator.DAL.Global.Entities;
+using C = Calculator.DAL.Client.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.DAL.Mapper
+{
+    public class MeasureMapper
+    {
+        public G.Measure ToGlobal(C.Measure entity)
+        {
+            return new G.Measure()
+            {
+                MeasureID = entity.MeasureID,
+                Date = entity.Date
+            };
+        }
+
+        public List<G.Measure> ToGlobal(List<C.Measure> entities)
+        {
+            List<G.Measure> measures = new List<G.Measure>();
+
+            foreach (C.Measure item in entities)
+            {
+                measures.Add(ToGlobal(item));
+            }
+
+            return measures;
+        }
+
+        public C.Measure ToClient(G.Measure entity)
+        {
+            return new C.Measure()
+            {
+                MeasureID = entity.MeasureID,
+                Date = entity.Date
+            };
+        }
+
+        public List<C.Measure> ToClient(List<G.Measure> entities)
+        {
+            List<C.Measure> measures = new List<C.Measure>();
+
+            foreach (G.Measure item in entities)
+            {
+                measures.Add(ToClient(item));
+            }
+
+            return measures;
+        }
+    }
+}
